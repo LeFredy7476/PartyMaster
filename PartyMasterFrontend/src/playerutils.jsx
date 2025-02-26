26a8c018fe0c8230810476839cdfc4cf237511a2
LeFredy7476 <forgetfrederick@gmail.com>
2025-02-26 10:31:12 -0500

    linked 3 missing icons

--- PartyMasterFrontend/src/playerutils.jsx
+++ PartyMasterFrontend/src/playerutils.jsx
@@ -4,6 +4,9 @@ import icon2 from '/players/icon-2.svg'
 import icon3 from '/players/icon-3.svg'
 import icon4 from '/players/icon-4.svg'
 import icon5 from '/players/icon-5.svg'
+import icon6 from '/players/icon-6.svg'
+import icon7 from '/players/icon-7.svg'
+import icon8 from '/players/icon-8.svg'
 
 const icons = [
     icon0,
@@ -11,7 +14,10 @@ const icons = [
     icon2,
     icon3,
     icon4,
-    icon5
+    icon5,
+    icon6,
+    icon7,
+    icon8
 ];
 
 export default function get_player_icon(icon_id) {
