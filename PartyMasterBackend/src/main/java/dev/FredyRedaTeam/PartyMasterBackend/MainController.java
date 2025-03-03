c1150d4eabb8058bcbdea95cc9b72a077c9ce202
LeFredy7476 <forgetfrederick@gmail.com>
2025-03-03 15:02:23 -0500

    mime type

--- PartyMasterBackend/src/main/java/dev/FredyRedaTeam/PartyMasterBackend/MainController.java
+++ PartyMasterBackend/src/main/java/dev/FredyRedaTeam/PartyMasterBackend/MainController.java
@@ -2,6 +2,7 @@ package dev.FredyRedaTeam.PartyMasterBackend;
 
 import dev.FredyRedaTeam.PartyMasterBackend.model.Lobby;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -11,7 +12,7 @@ public class MainController {
 
 
 
-    @GetMapping(value = "/lobby/{room}/state")
+    @GetMapping(value = "/lobby/{room}/state", produces = MediaType.APPLICATION_JSON_VALUE,)
     public String state(@PathVariable String room, @ModelAttribute("uuid") String uuid, HttpServletRequest request) {
         System.out.print("got request for |");
         System.out.print(room);
