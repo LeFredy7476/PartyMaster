3e5635a6a5664a4e6612ed3553c41b046ae8ccab
LeFredy7476 <forgetfrederick@gmail.com>
2025-03-19 11:00:26 -0400

    create room mapping

--- PartyMasterBackend/src/main/java/dev/FredyRedaTeam/PartyMasterBackend/MainController.java
+++ PartyMasterBackend/src/main/java/dev/FredyRedaTeam/PartyMasterBackend/MainController.java
@@ -1,7 +1,9 @@
 package dev.FredyRedaTeam.PartyMasterBackend;
 
 import dev.FredyRedaTeam.PartyMasterBackend.model.Lobby;
+import dev.FredyRedaTeam.PartyMasterBackend.model.Response;
 import jakarta.servlet.http.HttpServletRequest;
+import org.json.JSONObject;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
@@ -31,6 +33,14 @@ public class MainController {
         }
     }
 
+    @GetMapping(value = "/{room}/lobby/ping", produces = MediaType.APPLICATION_JSON_VALUE)
+    public String exist(@PathVariable String room, HttpServletRequest request) {
+        boolean doesExist = Lobby.isInstance(room);
+        JSONObject out = new JSONObject();
+        out.put("exist", doesExist);
+        return out.toString();
+    }
+
     @GetMapping(value = "{room}/lobby/tick", produces = MediaType.APPLICATION_JSON_VALUE)
     public String tick(@PathVariable String room, @ModelAttribute("uuid") String uuid, HttpServletRequest request) {
         if (Lobby.isInstance(room)) {
@@ -44,7 +54,11 @@ public class MainController {
 
     @PostMapping(value = "/createlobby")
     public String createLobby() {
-
+        Lobby lobby = new Lobby();
+        JSONObject data = new JSONObject();
+        data.put("lobby", lobby.getRoom());
+        Response out = new Response(0, data);
+        return out.toJson().toString();
     }
 
 }
