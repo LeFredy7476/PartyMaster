1469f5539ef0a81f3994279fb519ebb45c00518c
LeFredy7476 <forgetfrederick@gmail.com>
2025-03-23 22:40:25 -0400

    added the redirect for create lobby action

--- PartyMasterFrontend/src/MainMenu.jsx
+++ PartyMasterFrontend/src/MainMenu.jsx
@@ -2,9 +2,12 @@ import {useImmer} from 'use-immer'
 import './Lobby.css'
 import './Menu.css'
 import axios from 'axios'
-import {useParams} from "react-router-dom";
+import {useNavigate, useParams} from "react-router-dom";
 
 function MainMenu() {
+
+    const {room} = useParams();
+    let navigate = useNavigate();
 
     return (
         <main>
@@ -15,7 +18,15 @@ function MainMenu() {
                 <input type="text" name="lobbyCode" id="lobbyCode" placeholder="Code"/>
                 <button id="lobbyJoin">Rejoindre</button>
             </section>
-            <button id="lobbyCreate" className="form-button">Créer une partie</button>
+            <button id="lobbyCreate" className="form-button" onClick={function() {
+                // let prefix = ""
+                // if (window.location.protocol) {
+                //     prefix = window.Location.protocol
+                // }
+                console.log(window.location.protocol + "//" + window.location.hostname)
+                axios.post(window.location.protocol + "//" + window.location.hostname + ":");
+                navigate("/" + room);
+            }}>Créer une partie</button>
         </main>
     )
 }
