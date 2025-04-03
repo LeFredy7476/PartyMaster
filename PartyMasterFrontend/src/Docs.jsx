b5412e962dd03a37b688eb59f76f38ad35fc7095
SaciReda <reda211@outlook.com>
2025-04-03 17:07:11 -0400

    rajouter les infos sur les personnages et du css

--- PartyMasterFrontend/src/Docs.jsx
+++ PartyMasterFrontend/src/Docs.jsx
@@ -1,9 +1,75 @@
 import React from 'react'
+import { useNavigate, useParams } from "react-router-dom";
+
+function DocLoupgaroux() {
+    return (
+        <>
+            <h1>Aide pour le loup garou</h1>
+            <h2>Règle du jeu</h2>
+            <h3>Le concept du jeu du loup-garou est que, lorsque la nuit est tombée, les personnages possédant des capacités uniques, les utiliseront ou pour portée aide au village ou pour leur propre intérêt. À la suite de cela, les loups-garous se réveillent, et décident à l'unisson de manger un des villageois . Au matin, grâce au pouvoir de nos villageois, ils doivent dénicher les loups-garous et les pendre. </h3>
+            <h2>Role </h2>
+            <div className='grid'>
+                <div>
+                    <img width="100" height="150" src="/uno%20card/color.svg" alt="" />
+                    <h3>Villageois</h3>
+                    <p>simple gars qui doit deviner qui est les loups ,les tués , se faire dévorer par les loups ou traire des vaches</p>
+                </div>
+                <div> <img width="100" height="150" src="/uno%20card/blue%209.svg" alt="" />
+                <h3>loup garoux</h3>
+                <p>rester discret , dévorer des villageois sans vous faire prendre ou sinon la corde vous attends </p>
+                </div>
+               <div> <img width="100" height="150" src="/uno%20card/blue%208.svg" alt="" />
+               <h3>Voyante</h3>
+                <p>issue d'une famille de magicien roux, a la tombée de la nuit elle peut savoir le role d'un autre joueur lors de la partie</p>
+               </div>
+               
+                <div><img width="100" height="150" src="/uno%20card/green%209.svg" alt="" />
+                <h3>Traitre</h3>
+                <p>ayant pour idole le comte de talleyrand et judas, lors de la nuit, il prend la carte d'une personne au choix sans en prendre connaissance et l'envoie a un joueur aléatoire dans la partie</p>
+                </div>
+                
+                <div>
+                    <img width="100" height="150" src="/uno%20card/yellow%206.svg" alt="" />
+                    <h3>Cupidon</h3>
+                    <p>N'ayant jamais trouvé l'amour grace a son charme, cupidon a pour capacité de rendre deux personne amoureuse lors de la première journée ayant pour effet que si l'un des tourteraux meurt, son amant le suis dans le trépas</p>
+                </div>
+                <div>
+                    <img width="100" height="150" src="/uno%20card/red%202.svg" alt="" />
+                    <h3>Gardien</h3>
+                    <p>Une des rares personne avec un peu de bonne intention dans ce village, le gardien a pour capacité de protéger quelqu'un des loups garoux lors de la nuit,attention il ne peut pas proteger une personne deux fois de suite</p>
+                </div>
+                
+                <div>
+                    <img width="100" height="150" src="/uno%20card/blue%207.svg" alt="" />
+                    <h3>Chasseur</h3>
+                    <p>Un homme extremement rancunier et doté d'un fusil,le chasseur lorsque qu'il est pendu ou dévoré, choisis une personne qui mourreras avec lui </p>
+                </div>
+                <div>
+                   <img width="100" height="150" src="/uno%20card/yellow%206.svg" alt="" /> 
+                   <h3>Loup blanc</h3>
+                    <p>Agent double détenant une belle fourrure blanche, le loup blanc a pour mission de tué le village puis les loups afin de gagner en étant seul</p>
+                </div>
+                
+
+            </div>
+        </>
+    )
+}
 
 export default function Docs() {
-  return (
-    <main>
-      <p>info</p>
-    </main>
-  )
+    const { game } = useParams();
+    console.log(game)
+
+    var doc = <></>;
+    if (game == "loupgaroux") {
+        doc = <DocLoupgaroux />
+    }
+
+
+    return (
+        <main className='docs'>
+            {doc}
+        </main>
+    )
+
 }
